6cc3ad83cff6e4128de50ecf63e00ec66e99965c
kacevnik <kacevnik@yandex.ru>
2018-06-28 20:08:22 +0300

    Начальные файлы проекта

---
+++ header.php
@@ -0,0 +1,138 @@
+<?php
+/**
+ * The template for displaying the header
+ *
+ * Displays all of the head element and everything up until the "site-content" div.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<?php endif; ?>
+	<?php wp_head(); ?>
+</head>
+
+<style>
+.wp-post-image
+/* картинка на странице */
+.minimized {
+  width: 300px;
+  cursor: pointer;
+  border: 1px solid #FFF;
+}
+ 
+.wp-post-image:hover {
+  border: 1px solid yellow;
+}
+ 
+/* увеличенная картинка */
+#magnify {
+  display: none;
+ 
+  /* position: absolute; upd: 24.10.2016 */
+  position: fixed;
+  max-width: 600px;
+  height: auto;
+  z-index: 9999;
+}
+ 
+#magnify img {
+  width: 100%;
+}
+ 
+/* затемняющий фон */
+#overlay {
+  display: none;
+ 
+  background: #000;
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0.5;
+  z-index: 9990;
+}
+ 
+/* кнопка закрытия */
+#close-popup {
+  width: 30px;
+  height: 30px;
+ 
+  background: #FFFFFF;
+  border: 1px solid #AFAFAF;
+  border-radius: 15px;
+  cursor: pointer;
+  position: absolute;
+  top: 15px;
+  right: 15px;
+}
+ 
+#close-popup i {
+  width: 30px;
+  height: 30px;
+  background: url(https://codernote.ru/files/cross.png) no-repeat center center;
+  background-size: 16px 16px;
+  display: block;
+}
+ 
+@keyframes rota {
+ 25% { transform: rotate(360deg); }
+}
+ 
+#close-popup:hover {
+  animation: rota 4s infinite normal;
+  -webkit-animation-iteration-count: 1;
+  animation-iteration-count: 1;
+}
+</style>
+<link rel="stylesheet" href="/main.css">
+</head>
+<body>
+	<header style="background-image: url(/wp-content/uploads/2017/09/EXO250_1920_1080-e1511035945270.jpg);">
+		<div class="container">
+
+			<div class="row">
+				<div class="col-md-2 col-sm-2 col-sm-push-5 col-md-pull-5">
+					<a href="" rel="home" class="logo">
+							<img src="<?php the_field('logotype'); ?>" />
+					</a>
+				</div>
+				<div class="col-md-5 col-sm-5 col-sm-pull-2 col-md-pull-2 col-xs-6">
+					<div class="block__left">
+						<span class="socials__item">
+							<span><?php the_field('pr_time'); ?></span>
+							
+							<strong></strong>
+						</span>
+					</div>
+				</div>
+				<div class="col-md-5 col-sm-5 col-xs-6">
+					<div class="socials">
+						<a href="#" class="socials__item socials__item__email"><img src="/app/img/mail-icon.png" alt="Email"></a>
+						<a href="https://vk.com/shpakshenprodakshen" class="socials__item socials__item__vk"><img src="/app/img/vk-icon.png" alt="VK"></a>
+
+				<a href="#cart-popup" class="open-popup-link cart-customlocation cart__item">
+					<span>Ваша корзина:</span>
+					<strong><?php echo do_shortcode( ' [nc_ajax_cart] ' );?></strong>
+				</a>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</header>
+	<script type="text/javascript">
+		function myFunction(x) {
+			x.classList.toggle("change");
+		}
+	</script>
---
+++ page.php
@@ -0,0 +1,190 @@
+<?php
+/**
+ * The template for displaying pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other "pages" on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+
+get_header(); ?>
+
+<?php get_template_part( 'template-parts/featured-image' ); ?>
+<?php the_field('production_time'); ?>
+<section class="about">
+	<nav class="staticHeader">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="mobile-btn" onclick="myFunction(this);">
+						<div class="bar1"></div>
+						<div class="bar2"></div>
+						<div class="bar3"></div>
+					</div>
+					<ul>
+					<?php
+
+// check if the repeater field has rows of data
+if( have_rows('menu_m') ):
+
+ 	// loop through the rows of data
+    while ( have_rows('menu_m') ) : the_row();
+
+        // display a sub field value
+        echo'<li><a href="';the_sub_field('tmple_link');echo'" class="page-scroll">';the_sub_field('tmple_menu');echo'</a></li>';
+
+    endwhile;
+
+else :
+
+    // no rows found
+
+endif;
+
+?>
+					</ul>
+
+				</div>
+			</div>
+		</div>
+	</nav>
+	<div class="container">
+
+		<div class="row">
+			<div class="col-md-12">
+				<h2><?php the_field('block_about_me_tittle'); ?></h2>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-5 col-sm-6">
+				<div class="about__img">
+					<img src="<?php the_field('block_about_me_img'); ?>" >
+				</div>
+			</div>
+			<div class="col-md-7 col-sm-6">
+				<?php the_field('block_about_me_text'); ?>
+			</div>
+		</div>
+
+	</div>
+</section>
+
+
+
+<section class="product">
+	<div class="container">
+
+		<div class="row">
+			<div class="col-md-12">
+				<h2>Мои изделия</h2>
+			</div>
+		</div>
+
+		<!-- BEGIN BLOCK -->
+
+	<!-- END BLOCK -->
+
+	<!-- BEGIN BLOCK -->
+	<div class="row" id="block2">
+	<h3>Альбомы</h3>
+<?php echo do_shortcode("[product_category category='albomy' per_page='12']"); ?>
+
+	</div>
+
+<!-- END BLOCK -->
+<!-- BEGIN BLOCK -->
+<div class="row" id="block3">
+<h3>Блокноты</h3>
+<?php echo do_shortcode("[product_category category='bloknoty' ]"); ?>
+</div>
+<!-- END BLOCK -->
+<!-- BEGIN BLOCK -->
+<div class="row" id="block4">
+<h3>Кулинарные книги</h3>
+<?php echo do_shortcode("[product_category category='kulinarnye-knigi' ]"); ?>
+</div>
+<!-- END BLOCK -->
+
+<!-- BEGIN BLOCK -->
+<div class="row" id="block5">
+<h3>Мамины сокровища</h3>
+<?php echo do_shortcode("[product_category category='maminy-sokrovishha' ]"); ?>
+</div>
+<!-- END BLOCK -->
+<!-- BEGIN BLOCK -->
+<div class="row" id="block6">
+<h3>Обложки</h3>
+<?php echo do_shortcode("[product_category category='oblozhki' ]"); ?>
+</div>
+<!-- END BLOCK -->
+<!-- BEGIN BLOCK -->
+<div class="row" id="block7">
+<h3>Для свадьбы</h3>
+<?php echo do_shortcode("[product_category category='shkatulki' ]"); ?>
+</div>
+<!-- END BLOCK -->
+
+<!-- BEGIN BLOCK -->
+<div class="row" id="block8">
+<h3>Подарочные сертификаты</h3>
+<?php echo do_shortcode("[product_category category='sertifikaty-i-upakovka' ]"); ?>
+</div>
+
+</div>
+</section>
+
+<section class="issues">
+	<div class="container">
+
+		<div class="row">
+			<div class="col-md-12">
+				<h2><?php the_field('an_on_ques_tittle'); ?></h2>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-12">
+
+
+				<?php if( have_rows('an_on_ques') ): ?>
+
+					<?php while( have_rows('an_on_ques') ): the_row();
+
+							// vars
+
+					$title = get_sub_field('question');
+					$content = get_sub_field('answer');
+
+					?>
+
+					<div class="issues__item">
+						<span><?php echo $title; ?></span>
+						<div class="more-text">+</div>
+						<p><?php echo $content; ?></p>
+
+					</div>
+					
+					
+					
+					
+					
+
+				<?php endwhile; ?>
+
+			<?php endif; ?>
+
+			<p class="issues__descr"><?php the_field('delivery'); ?></p>
+
+		</div>
+		
+
+	</div>
+
+</div>
+</section>
+<?php get_footer(); ?>
